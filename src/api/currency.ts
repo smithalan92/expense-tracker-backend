68036f523cb1cd824c382a8bd9f996d8c88de623
Alan Smith <smithalan2@gmail.com>
2024-12-20 03:27:24 +0000

    Prettier & eslint

--- src/api/currency.ts
+++ src/api/currency.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 import { type GetCurrencyResponse } from './currency.types';
 
 export async function getExchangeRatesForEUR() {
-  const { data } = await axios.get<GetCurrencyResponse>('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json');
+  const { data } = await axios.get<GetCurrencyResponse>(
+    'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json',
+  );
 
   return data;
 }
